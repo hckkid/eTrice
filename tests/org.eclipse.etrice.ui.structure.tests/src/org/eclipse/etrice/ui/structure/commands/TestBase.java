df3c358c9057846ce59ec1e2f1f6dfc4152248c9
Henrik Rentz-Reichert <hrr@protos.de>
2011-01-31 17:39:29 +0100

    ui.structure.tests: added epl header

--- tests/org.eclipse.etrice.ui.structure.tests/src/org/eclipse/etrice/ui/structure/commands/TestBase.java
+++ tests/org.eclipse.etrice.ui.structure.tests/src/org/eclipse/etrice/ui/structure/commands/TestBase.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2010 protos software gmbh (http://www.protos.de).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * CONTRIBUTORS:
+ * 		Thomas Schuetz and Henrik Rentz-Reichert (initial contribution)
+ * 
+ *******************************************************************************/
+
 package org.eclipse.etrice.ui.structure.commands;
 
 import static org.junit.Assert.fail;
