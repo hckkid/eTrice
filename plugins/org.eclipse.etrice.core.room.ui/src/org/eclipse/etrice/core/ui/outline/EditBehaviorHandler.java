ac04515810b806b8e4abdbc7c93c709111e31885
Henrik Rentz-Reichert <hrr@protos.de>
2011-02-28 16:26:07 +0100

    core.room.ui: provide unnamed transitions with names and save xtext file before launching the diagram editor

--- plugins/org.eclipse.etrice.core.room.ui/src/org/eclipse/etrice/core/ui/outline/EditBehaviorHandler.java
+++ plugins/org.eclipse.etrice.core.room.ui/src/org/eclipse/etrice/core/ui/outline/EditBehaviorHandler.java
@@ -15,18 +15,38 @@ package org.eclipse.etrice.core.ui.outline;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.etrice.core.naming.RoomNameProvider;
 import org.eclipse.etrice.core.room.ActorClass;
+import org.eclipse.etrice.core.room.State;
+import org.eclipse.etrice.core.room.StateGraph;
+import org.eclipse.etrice.core.room.Transition;
+import org.eclipse.etrice.core.ui.internal.RoomActivator;
 import org.eclipse.etrice.ui.behavior.DiagramAccess;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.XtextEditor;
+import org.eclipse.xtext.ui.editor.model.IXtextDocument;
+import org.eclipse.xtext.ui.editor.model.edit.DefaultDocumentEditor;
 import org.eclipse.xtext.ui.editor.outline.ContentOutlineNode;
 import org.eclipse.xtext.ui.editor.utils.EditorUtils;
 import org.eclipse.xtext.util.concurrent.IUnitOfWork;
 
+import com.google.inject.Injector;
+
+/**
+ * Handler for outline menu item to open behavior editor.
+ * The handler will ask to save unsaved editor before opening the diagram.
+ * 
+ * If there are unnamed transitions they will be named with unique names.
+ *
+ * @author Henrik Rentz-Reichert initial contribution and API
+ *
+ */
 public class EditBehaviorHandler extends AbstractHandler {
 
 	@Override
@@ -38,6 +58,20 @@ public class EditBehaviorHandler extends AbstractHandler {
 			if (sel instanceof ContentOutlineNode) {
 				final ContentOutlineNode node = (ContentOutlineNode) sel;
 				XtextEditor xtextEditor = EditorUtils.getActiveXtextEditor(event);
+				if (xtextEditor.isDirty()) {
+					if (!MessageDialog.openQuestion(xtextEditor.getSite().getShell(), "Save model file", "The editor will be saved before opening the diagram editor.\nProceed?"))
+						return null;
+					// postpone save to avoid doing it twice
+				}
+				if (hasUnnamedTransitions(xtextEditor.getDocument(), node.getURI().fragment())) {
+					if (!MessageDialog.openQuestion(xtextEditor.getSite().getShell(), "Create transition names", "Transition names will be created where missing.\nProceed?"))
+						return null;
+					createTransitionNames(xtextEditor.getDocument(), node.getURI().fragment());
+					xtextEditor.doSave(new NullProgressMonitor());
+				}
+				if (xtextEditor.isDirty()) {
+					xtextEditor.doSave(new NullProgressMonitor());
+				}
 				xtextEditor.getDocument().readOnly(new IUnitOfWork.Void<XtextResource>() {
 					@Override
 					public void process(XtextResource resource) throws Exception {
@@ -53,6 +87,80 @@ public class EditBehaviorHandler extends AbstractHandler {
 			}
 		}
 		return null;
+	}
+
+	/**
+	 * @param document
+	 * @param fragment
+	 */
+	private void createTransitionNames(IXtextDocument document, final String fragment) {
+		DefaultDocumentEditor edit = new DefaultDocumentEditor();
+		Injector injector = RoomActivator.getInstance().getInjector("org.eclipse.etrice.core.Room");
+		injector.injectMembers(edit);
+		edit.process(new IUnitOfWork.Void<XtextResource>() {
+			@Override
+			public void process(XtextResource resource) throws Exception {
+				if (resource != null) {
+					EObject object = resource.getEObject(fragment);
+					if (object instanceof ActorClass) {
+						createTransitionNames(((ActorClass) object).getStateMachine());
+					}
+				}
+			}
+
+			private void createTransitionNames(StateGraph sg) {
+				if (sg==null)
+					return;
+				
+				for (Transition tr : sg.getTransitions()) {
+					if (tr.getName()==null || tr.getName().isEmpty()) {
+						tr.setName(RoomNameProvider.getUniqueTransitionName(sg));
+					}
+				}
+				
+				for (State s : sg.getStates()) {
+					createTransitionNames(s.getSubgraph());
+				}
+			}
+			
+		}, document);
+	}
+
+	/**
+	 * @param document
+	 * @param fragment 
+	 * @return
+	 */
+	private boolean hasUnnamedTransitions(IXtextDocument document, final String fragment) {
+		return document.readOnly(new IUnitOfWork<Boolean, XtextResource>() {
+			@Override
+			public Boolean exec(XtextResource resource) throws Exception {
+				if (resource != null) {
+					EObject object = resource.getEObject(fragment);
+					if (object instanceof ActorClass) {
+						return hasUnnamedTransitions(((ActorClass) object).getStateMachine());
+					}
+				}
+				return false;
+			}
+
+			private boolean hasUnnamedTransitions(StateGraph sg) {
+				if (sg==null)
+					return false;
+				
+				for (Transition tr : sg.getTransitions()) {
+					if (tr.getName()==null || tr.getName().isEmpty())
+						return true;
+				}
+				
+				for (State s : sg.getStates()) {
+					if (hasUnnamedTransitions(s.getSubgraph()))
+						return true;
+				}
+				
+				return false;
+			}
+		});
 	}
 
 }
