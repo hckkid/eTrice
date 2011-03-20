dc242108796a124db8dd32aaea8177fe8b573944
Thomas Schuetz <ts@protos.de>
2011-03-19 18:57:26 -0700

    added filter for MSCLogger

---
+++ runtime/org.eclipse.etrice.runtime.java/src/org/eclipse/etrice/runtime/java/debugging/MSCFilter.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2011 protos software gmbh (http://www.protos.de).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *******************************************************************************/
+
+
+package org.eclipse.etrice.runtime.java.debugging;
+
+import java.util.ArrayList;
+
+/**
+ * @author Thomas Schuetz
+ *
+ * Simple filtering that can be applied to MSCLogger to make the MSCs 
+ * 
+ * TODO: this is only a temporary solution for the current file based MSCLogger 
+ */
+
+public class MSCFilter {
+
+	public MSCFilter() {
+		filterList = new ArrayList<FilterItem>();
+	}
+	
+	
+	public void addFilter(FilterItem filter){
+		filterList.add(filter);
+	}
+
+	public boolean applyTo(String text){
+		for (FilterItem item : filterList){
+			if (text.startsWith(item.filter)) return true;
+		}
+		return false;
+	}
+
+	public String reduceString(String string){
+		if (filterList.size()==1)
+			return string.replaceFirst(filterList.get(0).filter, "");
+		else 
+			return string;
+	}
+	
+	private ArrayList<FilterItem> filterList= null;
+
+	// Sub classes
+	public static class FilterItem{
+		private boolean exclude = false;
+		private String filter = null;
+		public FilterItem(String filter, boolean exclude) {
+			super();
+			this.exclude = exclude;
+			this.filter = filter;
+		}
+	}
+	
+}
